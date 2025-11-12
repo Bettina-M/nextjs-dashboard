03520235d0c17bfe313bb037fb129035be95ea1e
Bettina-M <men23034@byui.edu>
2025-11-12 20:23:11 +0300

    dashboard complete

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
 export default function CustomerPage() {
     return <p>Customers Page</p>;
 } 
